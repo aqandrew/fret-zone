64c1475e74b0fbc6c80edf83f470f97cef355f24
dawneraq <dawneraq@gmail.com>
2020-04-03 15:04:50 -0700

    remove most of boilerplate and rename to React Guitar Pro

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <span role="img" aria-label="guitar">
+        🎸
+      </span>
+    </div>
+  );
+}
+
+export default App;

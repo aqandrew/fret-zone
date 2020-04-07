eea0b0ec1fe77f76d87fedab0870ac193b09197e
dawneraq <dawneraq@gmail.com>
2020-04-07 15:00:58 -0700

    add Add Track button to GlobalView

--- src/components/GlobalView.jsx
+++ src/components/GlobalView.jsx
@@ -2,6 +2,29 @@ import React from 'react';
 
 import './GlobalView.scss';
 
-const GlobalView = () => <div className="GlobalView">I'm the Global View!</div>;
+const GlobalView = () => {
+  // TODO How to share this function with AppMenu?
+  const openAddTrackModal = () => {
+    console.log('TODO open Add Track modal');
+  };
+
+  return (
+    <div className="GlobalView">
+      <div className="GlobalView__Controls--Top">
+        <button
+          className="GlobalView__Button--AddTrack"
+          title="Add Track"
+          onClick={openAddTrackModal}
+        >
+          +
+        </button>
+        <span className="GlobalView__Heading">Tracks</span>
+      </div>
+      <div className="GlobalView__Controls--Bottom">
+        <span className="GlobalView__Heading">Master</span>
+      </div>
+    </div>
+  );
+};
 
 export default GlobalView;

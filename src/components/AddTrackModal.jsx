204ea15813c8318fb1cb5b203dc07f9c1e89330d
dawneraq <dawneraq@gmail.com>
2020-04-13 18:17:37 -0700

    if there are other tracks when adding a track, add a new measure to this track for each measure that already exists

--- src/components/AddTrackModal.jsx
+++ src/components/AddTrackModal.jsx
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 
 import {
   addMeasure,
   addTrack,
-  defaultMeasureOptions
+  defaultMeasureOptions,
+  tracksSelector
 } from '../slices/document';
 
 import Modal from './Modal';
 
 const AddTrackModal = ({ show, onClose }) => {
   const dispatch = useDispatch();
+  const tracks = useSelector(tracksSelector);
   const defaultTrackOptions = {
     fullName: 'Electric Guitar - Clean',
     abbreviatedName: 'el.guit.',
@@ -27,14 +29,20 @@ const AddTrackModal = ({ show, onClose }) => {
     dispatch(addTrack({ id: newTrackId, ...trackToAdd }));
 
     // If this is the first track being added, add one new measure to this track
-    dispatch(
-      addMeasure({
-        trackId: newTrackId,
-        id: uuidv4(),
-        ...defaultMeasureOptions
-      })
-    );
-    // TODO Otherwise, if there are other tracks, add a new measure to this track for each measure that already exists
+    // Otherwise, if there are other tracks, add a new measure to this track for each measure that already exists
+    let numMeasuresToAdd = tracks.length === 0 ? 1 : tracks[0].measures.length;
+
+    // TODO This for-loop may not be the best approach if we want the ability to undo adding a track
+    for (let i = 0; i < numMeasuresToAdd; i++) {
+      dispatch(
+        addMeasure({
+          trackId: newTrackId,
+          id: uuidv4(),
+          ...defaultMeasureOptions
+        })
+      );
+    }
+
     // TODO Once the new track is added, set this as the currently selected track
   };
 

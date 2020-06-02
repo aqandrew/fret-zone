4d8532a088d74667b9b9c972fc46b888b5ef550f
dawneraq <dawneraq@gmail.com>
2020-06-02 16:48:11 -0700

    add Enter/Escape listeners to Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,36 +1,85 @@
-import React from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
 
 import './Modal.scss';
 
+// Container for modal component
 const Modal = ({ children, modalTitle, show, onClose, onConfirm }) => {
-  return show ? (
+  return (
+    show && (
+      <ShownModal
+        modalTitle={modalTitle}
+        onClose={onClose}
+        onConfirm={onConfirm}
+      >
+        {children}
+      </ShownModal>
+    )
+  );
+};
+
+// The actual modal component
+const ShownModal = ({ children, modalTitle, onClose, onConfirm }) => {
+  const modalRef = useRef(null);
+
+  const closeWithoutResult = useCallback(() => onClose(null), [onClose]);
+  const closeWithResult = useCallback(() => {
+    const modalResult = onConfirm();
+
+    onClose(modalResult);
+  }, [onClose, onConfirm]);
+
+  useEffect(() => {
+    if (!modalRef.current) {
+      return;
+    }
+
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        closeWithResult();
+      } else if (event.key === 'Escape') {
+        closeWithoutResult();
+      }
+
+      event.preventDefault();
+    };
+
+    const node = modalRef.current;
+    node.addEventListener('keydown', handleKeyDown);
+
+    // Select the first input in this modal,
+    // so that modalRef will hear the event
+    // TODO This may not make sense for every modal
+    node.querySelector('input').focus();
+
+    return () => {
+      node.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [closeWithoutResult, closeWithResult]);
+
+  return (
     // Even though the backdrop doesn't obscure the rest of the screen,
     // it prevents clicks outside until the modal is closed
     <div className="Modal__Backdrop">
-      <div className="Modal">
+      <div className="Modal" ref={modalRef}>
         <h2 className="Modal__Heading">{modalTitle}</h2>
         {children}
         <div className="Modal__Footer">
           <button
             className="Modal__FooterButton--Cancel"
-            onClick={() => onClose(null)}
+            onClick={closeWithoutResult}
           >
             Cancel
           </button>
           <button
             className="Modal__FooterButton--Confirm"
-            onClick={() => {
-              let modalResult = onConfirm();
-
-              onClose(modalResult);
-            }}
+            onClick={closeWithResult}
           >
             OK
           </button>
         </div>
       </div>
     </div>
-  ) : null;
+  );
 };
 
 export default Modal;

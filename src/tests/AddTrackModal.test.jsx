bef6bf081063a492c8a6ecbd480ddd416bf477fc
dawneraq <dawneraq@gmail.com>
2020-06-26 15:43:35 -0700

    add "functions that need to know the selected measure" suite to App.test.jsx
    - write getCurrentBarMaximumDuration test
    - move AddTrackModal tests into their own file

---
+++ src/tests/AddTrackModal.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  render,
+  fireEvent,
+  screen,
+  getByText,
+  getByLabelText,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+
+import store from '../store';
+import App from '../App';
+
+describe('AddTrackModal', () => {
+  it(`can be opened by clicking the '+' button in GlobalView`, () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTitle('Add Track'));
+
+    expect(
+      getByText(screen.getByRole('dialog'), 'Add Track')
+    ).toBeInTheDocument();
+  });
+
+  it('creates a new track on confirm', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+
+    expect(container.querySelector('.TrackControl')).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByTitle('Add Track'));
+
+    const firstInput = getByLabelText(
+      screen.getByRole('dialog'),
+      'Electric Guitar - Clean',
+      { exact: false }
+    );
+
+    expect(firstInput).toHaveFocus();
+
+    fireEvent.keyDown(firstInput, { key: 'Enter' });
+
+    expect(container.querySelector('.TrackControl')).toBeInTheDocument();
+  });
+});
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { unmountComponentAtNode } from 'react-dom';
 import {
   render,
   fireEvent,
@@ -14,52 +15,53 @@ import App from '../App';
 
 describe('App', () => {
   it('renders without throwing errors', () => {
-    render(
-      <Provider store={store}>
-        <App />
-      </Provider>
-    );
+    expect(() => {
+      render(
+        <Provider store={store}>
+          <App />
+        </Provider>
+      );
+    }).not.toThrow();
   });
 
-  describe('AddTrackModal', () => {
-    it(`can be opened by clicking the '+' button in GlobalView`, () => {
+  describe('functions that need to know the selected measure', () => {
+    it('getCurrentBarMaximumDuration', () => {
       render(
         <Provider store={store}>
           <App />
         </Provider>
       );
 
-      expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+      const barCurrentDuration = screen.getByTitle('Bar current duration');
 
+      // Default to 1.0
+      expect(barCurrentDuration).toHaveTextContent('0.0:1.0');
+
+      // Create a new track with default options
       fireEvent.click(screen.getByTitle('Add Track'));
-
-      expect(
-        getByText(screen.getByRole('dialog'), 'Add Track')
-      ).toBeInTheDocument();
-    });
-
-    it('creates a new track on confirm', () => {
-      const { container } = render(
-        <Provider store={store}>
-          <App />
-        </Provider>
-      );
-
-      expect(container.querySelector('.TrackControl')).not.toBeInTheDocument();
-
-      fireEvent.click(screen.getByTitle('Add Track'));
-
       const firstInput = getByLabelText(
         screen.getByRole('dialog'),
         'Electric Guitar - Clean',
         { exact: false }
       );
-
-      expect(firstInput).toHaveFocus();
-
       fireEvent.keyDown(firstInput, { key: 'Enter' });
 
-      expect(container.querySelector('.TrackControl')).toBeInTheDocument();
+      // Default to 4.0 once a track is created
+      expect(barCurrentDuration).toHaveTextContent('0.0:4.0');
+    });
+
+    it('getCurrentBarDuration', () => {
+      // 0.0 initially
+      // Add a note
+      // Expect 1.0 (default note is quarter)
+      // Add one measure
+      // Expect new measure's length also to be 0.0 initially
+    });
+
+    describe('onKeyDown', () => {
+      it('passes selectedMeasure to dispatchSelectNextDuration', () => {});
+      it('passes selectedMeasure to dispatchSelectPreviousDuration', () => {});
+      it('passes selectedMeasure to dispatchDeleteNote', () => {});
     });
   });
 });

a30306b41d580728357df6eb5f109f33381439c5
dawneraq <dawneraq@gmail.com>
2020-10-28 13:33:34 -0700

    remove Redux

--- src/slices/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import documentReducer from './document';
-
-const rootReducer = combineReducers({
-  document: documentReducer,
-});
-
-export default rootReducer;

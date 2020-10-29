281178e489aff4fbf48d79b7dc0da0a7cee86b44
dawneraq <dawneraq@gmail.com>
2020-10-28 22:41:32 -0700

    test: condense bar current duration into 1 test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -38,63 +38,35 @@ describe('App', () => {
     });
   });
 
-  describe('functions that need to know the selected measure', () => {
-    it('getCurrentBarMaximumDuration', () => {
-      render(<App />);
+  test('bar current duration', () => {
+    const { container } = render(<App />);
 
-      const barCurrentDuration = screen.getByTitle('Bar current duration');
+    const barCurrentDuration = screen.getByTitle('Bar current duration');
 
-      // Default to 1.0
-      expect(barCurrentDuration).toHaveTextContent('0.0:1.0');
+    expect(barCurrentDuration).toHaveTextContent('0.0:1.0');
 
-      // Create a new track with default options
-      fireEvent.click(screen.getByTitle('Add Track'));
-      const firstInput = getByLabelText(
-        screen.getByRole('dialog'),
-        'Clean Guitar',
-        { exact: false }
-      );
-      fireEvent.keyDown(firstInput, { key: 'Enter' });
+    // Create a new track with default options
+    fireEvent.click(screen.getByTitle('Add Track'));
+    const firstInput = getByLabelText(
+      screen.getByRole('dialog'),
+      'Clean Guitar',
+      { exact: false }
+    );
+    fireEvent.keyDown(firstInput, { key: 'Enter' });
 
-      // Default to 4.0 once a track is created
-      expect(barCurrentDuration).toHaveTextContent('0.0:4.0');
-    });
+    // Default time signature is 4/4
+    expect(barCurrentDuration).toHaveTextContent('0.0:4.0');
 
-    it('getCurrentBarDuration', () => {
-      const { container } = render(<App />);
+    fireEvent.keyDown(container, { key: '0' });
 
-      const barCurrentDuration = screen.getByTitle('Bar current duration');
+    // Default note is a quarter note
+    expect(barCurrentDuration).toHaveTextContent('1.0:4.0');
 
-      // 0.0 initially
-      expect(barCurrentDuration).toHaveTextContent('0.0:1.0');
+    // Add one measure
+    fireEvent.keyDown(container, { key: 'ArrowRight' });
+    fireEvent.keyDown(container, { key: 'ArrowRight' });
 
-      // Create a new track with default options
-      fireEvent.click(screen.getByTitle('Add Track'));
-      const firstInput = getByLabelText(
-        screen.getByRole('dialog'),
-        'Clean Guitar',
-        { exact: false }
-      );
-      fireEvent.keyDown(firstInput, { key: 'Enter' });
-
-      // Add a note
-      fireEvent.keyDown(container, { key: '0' });
-
-      // Expect 1.0 (default note is quarter)
-      expect(barCurrentDuration).toHaveTextContent('1.0:4.0');
-
-      // Add one measure
-      fireEvent.keyDown(container, { key: 'ArrowRight' });
-      fireEvent.keyDown(container, { key: 'ArrowRight' });
-
-      // Expect new measure's length also to be 0.0 initially
-      expect(barCurrentDuration).toHaveTextContent('0.0:4.0');
-    });
-
-    describe('onKeyDown', () => {
-      it('passes selectedMeasure to dispatchSelectNextDuration', () => {});
-      it('passes selectedMeasure to dispatchSelectPreviousDuration', () => {});
-      it('passes selectedMeasure to dispatchDeleteNote', () => {});
-    });
+    // Expect additional measure's length also to be 0.0 initially
+    expect(barCurrentDuration).toHaveTextContent('0.0:4.0');
   });
 });

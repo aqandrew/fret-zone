186c32286033458d6315102f5ce6631419e6a758
dawneraq <dawneraq@gmail.com>
2020-12-07 17:56:54 -0800

    remove unused import from App test

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  render,
-  fireEvent,
-  screen,
-  getByLabelText,
-} from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import App from '../App';
